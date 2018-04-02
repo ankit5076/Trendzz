3807bfe40decf80837566106da43a33ece55a658
Neeraj <neeraj.vr16@gmail.com>
2018-04-03 00:57:10 +0530

    Added receiver

---
+++ app/src/main/java/com/trends/trending/service/ReturnReceiver.java
@@ -0,0 +1,39 @@
+package com.trends.trending.service;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+
+/**
+ * Created by USER on 4/2/2018.
+ */
+
+public class ReturnReceiver extends ResultReceiver {
+
+    private Receiver mReceiver;
+
+    /**
+     * Create a new ResultReceive to receive results.  Your
+     * {@link #onReceiveResult} method will be called from the thread running
+     * <var>handler</var> if given, or from an arbitrary thread if null.
+     *
+     * @param handler
+     */
+    public ReturnReceiver(Handler handler) {
+        super(handler);
+    }
+
+    public void setReceiver(Receiver receiver) {
+        mReceiver = receiver;
+    }
+
+    public interface Receiver {
+        public void onReceiveResult(int resultCode, Bundle resultData);
+    }
+
+    @Override
+    protected void onReceiveResult(int resultCode, Bundle resultData) {
+        if (mReceiver != null) {
+            mReceiver.onReceiveResult(resultCode, resultData);
+        }
+    }
+}
