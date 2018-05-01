2486f85523dca7383b501ec8be2712077ff4adf8
Neeraj <neeraj.vr16@gmail.com>
2018-05-01 22:13:43 +0530

    class name changed

--- app/src/main/java/com/trends/trending/fragment/VideoFragment.java
+++ app/src/main/java/com/trends/trending/fragment/VideoFragment.java
@@ -11,6 +11,7 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -18,9 +19,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.google.gson.Gson;
 import com.trends.trending.R;
-import com.trends.trending.adapter.PlaylistAdapter;
 import com.trends.trending.adapter.VideoAdapter;
 import com.trends.trending.model.youtube.Item;
 import com.trends.trending.model.youtube.Parent;
@@ -33,16 +36,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.trends.trending.utils.ExtraHelper.PREFS_NAME;
+import static com.trends.trending.utils.Keys.VideoInfo.KEY_API;
 import static com.trends.trending.utils.Keys.VideoInfo.KEY_INTENT;
 import static com.trends.trending.utils.Keys.VideoInfo.KEY_RECEIVER;
-import static com.trends.trending.utils.Keys.VideoInfo.TAB_COMEDY;
-import static com.trends.trending.utils.Keys.VideoInfo.TAB_FITNESS;
-import static com.trends.trending.utils.Keys.VideoInfo.TAB_MUSIC;
-import static com.trends.trending.utils.Keys.VideoInfo.TAB_NEWS;
-import static com.trends.trending.utils.Keys.VideoInfo.TAB_TECHNOLOGY;
 import static com.trends.trending.utils.Keys.VideoInfo.TAB_TRAILER;
 import static com.trends.trending.utils.Keys.VideoInfo.TAB_TRENDING;
-import static com.trends.trending.utils.Keys.VideoInfo.TAB_VINES;
 import static com.trends.trending.utils.Keys.VideoInfo.VAL_SEARCH;
 import static com.trends.trending.utils.Keys.VideoInfo.VAL_TRENDING;
 
@@ -55,6 +53,7 @@ public class VideoFragment extends Fragment  {
     private List<Item> videoList = new ArrayList<>();
     private List<SearchItem> searchVideoList = new ArrayList<>();
     private View view;
+    private YouTubePlayer YPlayer;
 
     SharedPreferences settings;
 
@@ -85,6 +84,31 @@ public class VideoFragment extends Fragment  {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_video, container, false);
+        //YouTubePlayerSupportFragment youTubePlayerFragment = (YouTubePlayerSupportFragment) getActivity().getFragmentManager().findFragmentById(R.id.youtube_fragment);
+        YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.youtube_fragment, youTubePlayerFragment).commit();
+        youTubePlayerFragment.initialize(KEY_API, new YouTubePlayer.OnInitializedListener() {
+
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                if (!b) {
+                    YPlayer = youTubePlayer;
+//                    YPlayer.setFullscreen(false);
+//                    YPlayer.loadVideo("2zNSgSzhBfM");
+//                    YPlayer.play();
+
+                    YPlayer.cueVideo("2zNSgSzhBfM");
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
+
         mReturnReceiver = new ReturnReceiver(new Handler());
         mReturnReceiver.setReceiver((ReturnReceiver.Receiver) getContext());
         setRecyclerView();
@@ -99,8 +123,8 @@ public class VideoFragment extends Fragment  {
         String[] musicTitle = getResources().getStringArray(R.array.music_chhanel_title);
         TypedArray musicImage = getResources().obtainTypedArray(R.array.music_chhanel_image);
 
-        String[] otherTitle = getResources().getStringArray(R.array.others_chhanel_title);
-        TypedArray otherImage = getResources().obtainTypedArray(R.array.other_chhanel_image);
+//        String[] otherTitle = getResources().getStringArray(R.array.others_chhanel_title);
+//        TypedArray otherImage = getResources().obtainTypedArray(R.array.other_chhanel_image);
 
         channelList.clear();
 
@@ -182,7 +206,5 @@ public class VideoFragment extends Fragment  {
         Bundle args = getArguments();
         return args.getString("title", "NO TITLE FOUND");
     }
-
-
 
 }
