7623058ff064378fdd39424d637efada9e3282b5
Neeraj <neeraj.vr16@gmail.com>
2018-08-11 23:01:21 +0530

    added ad view

--- app/src/main/java/com/trends/trending/adapter/SongAdapter.java
+++ app/src/main/java/com/trends/trending/adapter/SongAdapter.java
@@ -3,39 +3,45 @@ package com.trends.trending.adapter;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.NativeExpressAdView;
 import com.trends.trending.R;
-import com.trends.trending.custom.GifImageView;
-import com.trends.trending.model.PlaceToVisitModel;
 import com.trends.trending.model.SongModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SongAdapter extends
-        RecyclerView.Adapter<SongAdapter.ViewHolder> {
+        RecyclerView.Adapter {
 
     private static final String TAG = SongAdapter.class.getSimpleName();
+    private static final int DATA_VIEW_TYPE = 0;
+    private static final int NATIVE_EXPRESS_AD_VIEW_TYPE = 1;
 
     private Context context;
-    private List<SongModel> list;
+    private List<Object> list = new ArrayList<>();
     private OnItemClickListener mItemClickListener;
+    private int spaceBetweenAds;
 
 
     public SongAdapter(Context context, List<SongModel> list) {
         this.context = context;
-        this.list = list;
+        this.list.addAll(list);
+    }
+    public SongAdapter(Context context, ArrayList<Object> list, int spaceBetweenAds) {
+        this.context = context;
+        this.list.addAll(list);
+        this.spaceBetweenAds = spaceBetweenAds;
     }
 
     public interface OnItemClickListener {
@@ -64,7 +70,7 @@ public class SongAdapter extends
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    mItemClickListener.onItemClick(itemView, getAdapterPosition(), list.get(getAdapterPosition()));
+                    mItemClickListener.onItemClick(itemView, getAdapterPosition(), (SongModel) list.get(getAdapterPosition()));
                 }
             });
 
@@ -72,32 +78,96 @@ public class SongAdapter extends
 
     }
 
+    // View Holder for Admob Native Express Ad Unit
+    public class NativeExpressAdViewHolder extends RecyclerView.ViewHolder {
+        NativeExpressAdViewHolder(View view) {
+            super(view);
+        }
+    }
+
+
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = inflater.inflate(R.layout.item_top_ten, parent, false);
-        ButterKnife.bind(this, view);
+        switch (viewType) {
+            case DATA_VIEW_TYPE:
+                View dataLayoutView = LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.item_top_ten, parent, false);
+                ButterKnife.bind(this, dataLayoutView);
+                return new ViewHolder(dataLayoutView);
+            case NATIVE_EXPRESS_AD_VIEW_TYPE:
+                // fall through
+            default:
+                View nativeExpressLayoutView = LayoutInflater.from(
+                        parent.getContext()).inflate(R.layout.card_native_express_ad_container,
+                        parent, false);
+                return new NativeExpressAdViewHolder(nativeExpressLayoutView);
+        }
 
-        ViewHolder viewHolder = new ViewHolder(view);
 
-        return viewHolder;
+
+
+//        Context context = parent.getContext();
+//        LayoutInflater inflater = LayoutInflater.from(context);
+//
+//        View view = inflater.inflate(R.layout.item_top_ten, parent, false);
+//        ButterKnife.bind(this, view);
+//
+//        ViewHolder viewHolder = new ViewHolder(view);
+//
+//        return viewHolder;
     }
 
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        SongModel song = list.get(position);
-        holder.divider.setBackgroundColor(getRandomMaterialColor("400"));
-        holder.songName.setText(song.getSongName());
-        holder.movieName.setText(song.getMovieName());
-//        holder.songViews.setText(context.getResources().getString(R.string.total_views, song.getSongViews()));
-        holder.songViews.setText(song.getSongViews());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        int viewType = getItemViewType(position);
+
+        switch (viewType) {
+            case DATA_VIEW_TYPE:
+                ViewHolder dataViewHolder = (ViewHolder) holder;
+                SongModel song = (SongModel) list.get(position);
+                dataViewHolder.divider.setBackgroundColor(getRandomMaterialColor("400"));
+                dataViewHolder.songName.setText(song.getSongName());
+                dataViewHolder.movieName.setText(song.getMovieName());
+//              holder.songViews.setText(context.getResources().getString(R.string.total_views, song.getSongViews()));
+                dataViewHolder.songViews.setText(song.getSongViews());
+                break;
+            case NATIVE_EXPRESS_AD_VIEW_TYPE:
+                // fall through
+            default:
+                NativeExpressAdViewHolder nativeExpressHolder = (NativeExpressAdViewHolder) holder;
+                AdView adView = (AdView) list.get(position);
+                ViewGroup adCardView = (ViewGroup) nativeExpressHolder.itemView;
+
+                if (adCardView.getChildCount() > 0) {
+                    adCardView.removeAllViews();
+                }
+                if (adView.getParent() != null) {
+                    ((ViewGroup) adView.getParent()).removeView(adView);
+                }
+                adCardView.addView(adView);
+        }
+
+//        SongModel song = list.get(position);
+//        holder.divider.setBackgroundColor(getRandomMaterialColor("400"));
+//        holder.songName.setText(song.getSongName());
+//        holder.movieName.setText(song.getMovieName());
+////        holder.songViews.setText(context.getResources().getString(R.string.total_views, song.getSongViews()));
+//        holder.songViews.setText(song.getSongViews());
 
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        // Logic for returning view type based on spaceBetweenAds variable
+        // Here if remainder after dividing the position with (spaceBetweenAds + 1) comes equal to spaceBetweenAds,
+        // then return NATIVE_EXPRESS_AD_VIEW_TYPE otherwise DATA_VIEW_TYPE
+        // By the logic defined below, an ad unit will be showed after every spaceBetweenAds numbers of data items
+        return (position % (spaceBetweenAds + 1) == spaceBetweenAds) ? NATIVE_EXPRESS_AD_VIEW_TYPE: DATA_VIEW_TYPE;
+    }
 
     @Override
     public int getItemCount() {
--- app/src/main/java/com/trends/trending/ui/TopTen.java
+++ app/src/main/java/com/trends/trending/ui/TopTen.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.NativeExpressAdView;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -35,15 +41,18 @@ public class TopTen extends YouTubeBaseActivity implements
         YouTubePlayer.OnInitializedListener {
 
     private static final int RECOVERY_DIALOG_REQUEST = 1;
+    public final static int spaceBetweenAds = 3;
 
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
     @BindView(R.id.youtube_player)
     YouTubePlayerView mYouTubePlayerView;
 
-    private ArrayList<SongModel> mSongModels = new ArrayList<>();
+    private ArrayList<Object> mSongModels = new ArrayList<>();
     private String videoId;
     private YouTubePlayer mYoutubePlayer;
+
+
 
     protected String getVideoId() {
         return videoId;
@@ -87,7 +96,8 @@ public class TopTen extends YouTubeBaseActivity implements
             Toast.makeText(this, "null", Toast.LENGTH_SHORT).show();
         }
 
-        SongAdapter adapter = new SongAdapter(TopTen.this, mSongModels);
+        addNativeExpressAds();
+        SongAdapter adapter = new SongAdapter(TopTen.this, mSongModels, spaceBetweenAds);
         adapter.SetOnItemClickListener(new SongAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position, SongModel model) {
@@ -105,6 +115,90 @@ public class TopTen extends YouTubeBaseActivity implements
         recyclerView.setAdapter(adapter);
     }
 
+    private void addNativeExpressAds() {
+
+        // We are looping through our original dataset
+        // And adding Admob's Native Express Ad at consecutive positions at a distance of spaceBetweenAds
+        // You should change the spaceBetweenAds variable according to your need
+        // i.e how often you want to show ad in RecyclerView
+
+        for (int i = spaceBetweenAds; i <= mSongModels.size(); i += (spaceBetweenAds + 1)) {
+            AdView adView = new AdView(this);
+            // I have used a Test ID provided by Admob below
+            // you should replace it with yours
+            // And if wou are just experimenting, then just copy the code
+            adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+            mSongModels.add(i, adView);
+        }
+
+        // Below we are using post on RecyclerView
+        // because we want to resize our native ad's width equal to screen width
+        // and we should do it after RecyclerView is created
+
+        recyclerView.post(new Runnable() {
+            @Override
+            public void run() {
+                float scale = TopTen.this.getResources().getDisplayMetrics().density;
+                int adWidth = (int) (recyclerView.getWidth() - (2 * TopTen.this.getResources().getDimension(R.dimen.activity_horizontal_margin)));
+
+                // we are setting size of adView
+                // you should check admob's site for possible ads size
+                AdSize adSize = new AdSize((int) (adWidth / scale), 60);
+
+                // looping over mDataset to sesize every Native Express Ad to ew adSize
+                for (int i = spaceBetweenAds; i <= mSongModels.size(); i += (spaceBetweenAds + 1)) {
+                    AdView adViewToSize = (AdView) mSongModels.get(i);
+                    adViewToSize.setAdSize(AdSize.BANNER);
+                }
+
+                // calling method to load native ads in their views one by one
+                loadNativeExpressAd(spaceBetweenAds);
+            }
+        });
+
+    }
+
+    private void loadNativeExpressAd(final int index) {
+
+        if (index >= mSongModels.size()) {
+            return;
+        }
+
+        Object item = mSongModels.get(index);
+        if (!(item instanceof AdView)) {
+            throw new ClassCastException("Expected item at index " + index + " to be a Native"
+                    + " Express ad.");
+        }
+
+        final AdView adView = (AdView) item;
+
+        // Set an AdListener on the NativeExpressAdView to wait for the previous Native Express ad
+        // to finish loading before loading the next ad in the items list.
+        adView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                // The previous Native Express ad loaded successfully, call this method again to
+                // load the next ad in the items list.
+                loadNativeExpressAd(index + spaceBetweenAds + 1);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // The previous Native Express ad failed to load. Call this method again to load
+                // the next ad in the items list.
+                Log.e("AdmobMainActivity", "The previous Native Express ad failed to load. Attempting to"
+                        + " load the next Native Express ad in the items list.");
+                loadNativeExpressAd(index + spaceBetweenAds + 1);
+            }
+        });
+
+        // Load the Native Express ad.
+        //We also registering our device as Test Device with addTestDevic("ID") method
+        adView.loadAd(new AdRequest.Builder().addTestDevice("54F2A2C6318B029B2338389DB10AFDBE").build());
+    }
+
+
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
 
@@ -112,7 +206,8 @@ public class TopTen extends YouTubeBaseActivity implements
             youTubePlayer.loadVideo(getVideoId());
             mYoutubePlayer = youTubePlayer;
             // Hiding player controls
-//            youTubePlayer.setPlayerStyle(YouTubePlayer.PlayerStyle.CHROMELESS);
+//            youTubePlayer.setFullscreen(false);
+            youTubePlayer.setPlayerStyle(YouTubePlayer.PlayerStyle.MINIMAL);
         }
     }
 
@@ -127,6 +222,8 @@ public class TopTen extends YouTubeBaseActivity implements
             Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
         }
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
